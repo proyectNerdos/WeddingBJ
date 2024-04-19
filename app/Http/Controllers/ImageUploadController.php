ad89eeda191eb4afc8a17304bd4e5698ff1c34f9
sebasmaturanac <sebasmaturanac@gmail.com>
2024-04-18 23:06:55 -0300

    imagesize

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -10,7 +10,7 @@ class ImageUploadController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:50000000000',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:50000',
         ]);
 
 
